b002f3ae82b8b4d9ba092d9d44b3c4e12622169e
김민경 <mkkim@4dreplay.com>
2023-08-28 14:29:13 +0900

    Feature build by esbuild

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,20 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.divide = exports.multiply = exports.subtract = exports.add = void 0;
+// src/index.ts
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
-exports.add = add;
 function subtract(a, b) {
-    return a - b;
+  return a - b;
 }
-exports.subtract = subtract;
 function multiply(a, b) {
-    return a * b;
+  return a * b;
 }
-exports.multiply = multiply;
 function divide(a, b) {
-    return a / b;
+  return a / b;
 }
-exports.divide = divide;
+export {
+  add,
+  divide,
+  multiply,
+  subtract
+};
+//# sourceMappingURL=index.js.map
