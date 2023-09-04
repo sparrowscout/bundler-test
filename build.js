665640ffeae3f6b5ef28bf3d501957a814921cdf
김민경 <mkkim@4dreplay.com>
2023-09-04 14:10:16 +0900

    add onStart and onEnd

--- build.js
+++ build.js
@@ -1,6 +1,27 @@
 // build.js
 import esbuild from "esbuild";
 
+const onStartPlugin = {
+  name: "start",
+  setup(build) {
+    build.onStart(() => {
+      const startDate = new Date();
+      console.log(`build started ${startDate}`);
+    });
+  },
+};
+
+const onEndPlugin = {
+  name: "end",
+  setup(build) {
+    build.onEnd((result) => {
+      const endDate = new Date();
+      console.log(`build ended with ${result.errors.length} errors`);
+      console.log(`build ended ${endDate}`);
+    });
+  },
+};
+
 // 공통으로 사용할 옵션들
 // https://esbuild.github.io/api/#build 에서 다양한 옵션들을 확인할 수 있다.
 const baseConfig = {
@@ -12,6 +33,7 @@ const baseConfig = {
   mangleQuoted: true,
   minifySyntax: true,
   treeShaking: true,
+  plugins: [onStartPlugin, onEndPlugin],
 };
 Promise.all([
   // 한 번은 cjs
@@ -22,6 +44,7 @@ Promise.all([
       ".js": ".cjs",
     },
   }),
+
   // 한 번은 esm
   esbuild.build({
     ...baseConfig,
@@ -31,3 +54,23 @@ Promise.all([
   console.log("Build failed");
   process.exit(1);
 });
+
+let envPlugin = {
+  name: "env",
+  setup(build) {
+    // Intercept import paths called "env" so esbuild doesn't attempt
+    // to map them to a file system location. Tag them with the "env-ns"
+    // namespace to reserve them for this plugin.
+    build.onResolve({ filter: /^env$/ }, (args) => ({
+      path: args.path,
+      namespace: "env-ns",
+    }));
+
+    // Load paths tagged with the "env-ns" namespace and behave as if
+    // they point to a JSON file containing the environment variables.
+    build.onLoad({ filter: /.*/, namespace: "env-ns" }, () => ({
+      contents: JSON.stringify(process.env),
+      loader: "json",
+    }));
+  },
+};
