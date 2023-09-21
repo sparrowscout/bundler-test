715dc87319e6dc498ab9dc8ed1f59f92429678b4
김민경 <sparrowscout.0000@gmail.com>
2023-09-21 20:18:28 +0900

    Update npm scripts

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,1 @@
-function n(n, r) {
-  return n + r;
-}
-function r(n, r) {
-  return n - r;
-}
-function t(n, r) {
-  return n * r;
-}
-function u(n, r) {
-  return n / r;
-}
-export { n as add, u as divide, t as multiply, r as subtract };
+function n(n,r){return n+r}function r(n,r){return n-r}function t(n,r){return n*r}function u(n,r){return n/r}export{n as add,u as divide,t as multiply,r as subtract};
