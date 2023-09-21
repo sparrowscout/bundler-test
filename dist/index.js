52af3152611e9e3b92f86ba4bc55dd87ba9b3be5
김민경 <sparrowscout.0000@gmail.com>
2023-09-21 19:19:04 +0900

    Update tsconfig and gitignore

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,1 @@
-function u(n, r) {
-  return n + r;
-}
-function e(n, r) {
-  return n - r;
-}
-function t(n, r) {
-  return n * r;
-}
-function b(n, r) {
-  return n / r;
-}
-export { u as add, b as divide, t as multiply, e as subtract };
+function u(n,r){return n+r}function e(n,r){return n-r}function t(n,r){return n*r}function b(n,r){return n/r}export{u as add,b as divide,t as multiply,e as subtract};
