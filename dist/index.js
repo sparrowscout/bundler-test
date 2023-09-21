5bb6df4c28867c38e4e2d4a285ad606da9aad34b
김민경 <sparrowscout.0000@gmail.com>
2023-09-21 19:18:06 +0900

    Update tsconfig

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,28 @@
-var t={607:(t,r,e)=>{var o,n,i;!function(u){if("object"==typeof t.exports){var d=u(e(875),r);void 0!==d&&(t.exports=d)}else n=[e,r],void 0===(i="function"==typeof(o=u)?o.apply(r,n):o)||(t.exports=i)}((function(t,r){Object.defineProperty(r,"__esModule",{value:!0}),r.divide=r.multiply=r.subtract=r.add=void 0,r.add=function(t,r){return t+r},r.subtract=function(t,r){return t-r},r.multiply=function(t,r){return t*r},r.divide=function(t,r){return t/r}}))},875:t=>{function r(t){var r=new Error("Cannot find module '"+t+"'");throw r.code="MODULE_NOT_FOUND",r}r.keys=()=>[],r.resolve=r,r.id=875,t.exports=r}},r={};function e(o){var n=r[o];if(void 0!==n)return n.exports;var i=r[o]={exports:{}};return t[o](i,i.exports,e),i.exports}e.o=(t,r)=>Object.prototype.hasOwnProperty.call(t,r),e(607);
+var r = {
+    d: (t, e) => {
+      for (var n in e)
+        r.o(e, n) &&
+          !r.o(t, n) &&
+          Object.defineProperty(t, n, { enumerable: !0, get: e[n] });
+    },
+    o: (r, t) => Object.prototype.hasOwnProperty.call(r, t),
+  },
+  t = {};
+function e(r, t) {
+  return r + t;
+}
+function n(r, t) {
+  return r - t;
+}
+function o(r, t) {
+  return r * t;
+}
+function a(r, t) {
+  return r / t;
+}
+r.d(t, { $X: () => n, IH: () => e, Jp: () => o, cs: () => a });
+var u = t.IH,
+  c = t.cs,
+  i = t.Jp,
+  p = t.$X;
+export { u as add, c as divide, i as multiply, p as subtract };
